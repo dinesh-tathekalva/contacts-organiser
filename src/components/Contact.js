99374e60741b3ae5f823bdd2f2729efbe58f5524
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-07-30 20:17:15 -0500

    context uodated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Consumer } from '../context'
 import './../contact.css'
 
 class Contact extends Component {
@@ -7,39 +8,47 @@ class Contact extends Component {
         showContactInfo: false,
     }
 
-    onDeleteClicked = () => {
-        this.props.deleteClickHandler()
+    onDeleteClicked = (id, dispatch) => {
+        dispatch({type: 'DELETE_CONTACT', payload: id})
     }
     
     render() {
-        const { name, email, phone } = this.props.contact;
+        const { id, name, email, phone } = this.props.contact;
         const { showContactInfo } = this.state
         return (
-            <div className="card card-body mb-3">
-                <h4>
-                    {name}{' '}
-                    <i
-                        onClick={() =>
-                            this.setState({ showContactInfo: !this.state.showContactInfo })
-                        }
-                        className="fa fa-sort-down"
-                    />
-              <i className="fa fa-trash float-right text-danger" onClick={this.onDeleteClicked}/>
-
-                </h4>
-                {showContactInfo ? (<ul className="list-group">
-                    <li className="list-group-item">Email: {email}</li>
-                    <li className="list-group-item">Phone: {phone}</li>
-                </ul>) : null}
-                
-            </div>
+            <Consumer>
+                {value => {
+                    const { dispatch } = value
+                    return(
+                        <div className="card card-body mb-3">
+                        <h4>
+                            {name}{' '}
+                            <i
+                                onClick={() =>
+                                    this.setState({ showContactInfo: !this.state.showContactInfo })
+                                }
+                                className="fa fa-sort-down"
+                            />
+                            <i className="fa fa-trash float-right text-danger" 
+                            onClick={this.onDeleteClicked.bind(this, id, dispatch)}/>
+        
+                        </h4>
+                        {showContactInfo ? (<ul className="list-group">
+                            <li className="list-group-item">Email: {email}</li>
+                            <li className="list-group-item">Phone: {phone}</li>
+                        </ul>) : null}
+                        
+                    </div>
+                    )
+                }}
+            </Consumer>
+         
         )
     }
 }
 
 Contact.propTypes = {
     contact: PropTypes.object.isRequired,
-    deleteClickHandler: PropTypes.func.isRequired
 };
 
 export default Contact;
